3ce8f2d398842d0c43da1875a376d85ae5ced741
Anton Pysarenko <anton1980.00@gmail.com>
2020-06-14 16:45:55 +0300

    first commit

--- Required.js
+++ Required.js
@@ -1,14 +1,13 @@
-var _head = document.head
-var container = document.body
 var tags = ["header", "footer", "main", "div", "p"]
 var classes = ['class1', 'class2', 'class3', 'class4', 'class5']
-var style = _head.appendChild(document.createElement ('style'))
-for (let clas of classes) {
+var style = document.head.appendChild(document.createElement ('style'))
+for (var clas of classes) {
     style.appendChild (
         document.createTextNode (`.${clas} { color: blue; }`)
     )
 }
-for (let tag of tags) {
-    container.appendChild(document.createElement(tag))
-
+for (var i = 0; i < tags.length; i++) {
+  var tag = document.createElement(tags[i])
+  document.body.appendChild(tag)
+  tag.className = classes[i]
 }
